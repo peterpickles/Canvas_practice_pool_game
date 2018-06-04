ddb77f226aefcf54cc3b9e53422cf9c1a52bc0da
peterpickles <lipeter2006@gmail.com>
2018-06-03 23:45:44 -0700

    setting up scripts in the html

--- Game.js
+++ Game.js
@@ -1,7 +1,22 @@
 function Game() {}
 
-Game.prototype.init = function() {};
+Game.prototype.init = function() {
+  this.gameWorld = new GameWorld();
+};
 
-Game.prototype.start = function() {};
+Game.prototype.start = function() {
+  this.init();
+  this.mainLoop();
 
-Game.prototype.mainLoop = function() {};
+};
+
+Game.prototype.mainLoop = function() {
+  Canvas.clear();
+  PoolGame.gameWorld*();
+  PoolGame.gameWorld.draw();
+  requestAnimationFrame(PoolGame.mainLoop);
+};
+
+let PoolGame = new Game() {
+
+}
